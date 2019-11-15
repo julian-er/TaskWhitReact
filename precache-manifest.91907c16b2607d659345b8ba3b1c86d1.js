3ee5828e0588b0efb0875d78b7f585fada99ceba
JulianRosalen <rosalenjulian@gmail.com>
2019-11-15 00:34:39 -0300

    Updates

---
+++ precache-manifest.91907c16b2607d659345b8ba3b1c86d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fbad026d8b315fb035c9cbbda41a4b0",
+    "url": "/TaskWhitReact/index.html"
+  },
+  {
+    "revision": "db8c48c4a77b51f1cac6",
+    "url": "/TaskWhitReact/static/css/main.97474150.chunk.css"
+  },
+  {
+    "revision": "74de76ae2d8d9fc5b3c4",
+    "url": "/TaskWhitReact/static/js/2.210bd794.chunk.js"
+  },
+  {
+    "revision": "db8c48c4a77b51f1cac6",
+    "url": "/TaskWhitReact/static/js/main.ceeec989.chunk.js"
+  },
+  {
+    "revision": "10f438e5fbfbde6ca4ec",
+    "url": "/TaskWhitReact/static/js/runtime-main.c04c1144.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/TaskWhitReact/static/media/logo.25bf045c.svg"
+  }
+]);
